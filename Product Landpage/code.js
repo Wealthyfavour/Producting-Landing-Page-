240ea157cdf09a132cc11ac2ab4e3f165b99b57f
Wealthyfavour <123441733+Wealthyfavour@users.noreply.github.com>
2023-03-26 11:39:48 +0100

    Add files via upload

--- Product Landpage/code.js
+++ Product Landpage/code.js
@@ -1,18 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Javascript</title>
-</head>
-<body>
-    <script>
-        let form=document.getElementsByTagName("form")[0];
-        form.addEventListener("submit", (e)=> {
-            e.preventDefault();
-            alert("Subscribed!");
-        });
-      </script>
-</body>
-</html>
+<script>
+let form=document.getElementsByTagName("form")[0];
+form.addEventListener("submit", (e)= {
+    e.preventDefault()};
+    alert("Subscribed!");
+    );
+</script>
